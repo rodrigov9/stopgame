76e2cf9675199324e0c29ec352b19fed156457cf
Rodrigo Vitorino <49363242+rodev9@users.noreply.github.com>
2026-04-14 18:42:18 +0100

    chore(server): use pino-pretty only on development

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,19 +1,21 @@
-import { fastify } from 'fastify'
+import { fastify, FastifyServerOptions } from 'fastify'
 import { env } from './env.js'
 
 import { http } from './http/register.js'
 import { websockets } from './ws/register.js'
 
-const app = fastify({
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname'
-      }
+const devLoggerOptions = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      translateTime: 'HH:MM:ss Z',
+      ignore: 'pid,hostname'
     }
   }
+} satisfies FastifyServerOptions['logger']
+
+const app = fastify({
+  logger: env.NODE_ENV === 'development' ? devLoggerOptions : true
 })
 
 app.register(http)
